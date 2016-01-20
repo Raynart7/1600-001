cada34b4dd20dd7cc51557d5293a29f7f4b406b6
unknown <10693640@CS514-16.ad.uvu.edu>
2016-01-20 08:36:52 -0700

    no message

---
+++ DGM 1600 unity/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+	int myInt = 2000;
+	
+	void start()
+	{
+		
+	}
+	
+	// Use this for initialization
+	void update () 
+	{
+		myInt = DivideByTwo(myInt);
+		Debug.Log (myInt);
+	}
+	
+	int DivideByTwo (int number)
+	{
+		int ret;
+		ret = number / 2;
+		return ret;
+	}
+}
